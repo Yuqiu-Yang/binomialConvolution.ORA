274b25de93e81788149dccbd3ec902bb6d1c4bc9
Yuqiu (Ian) Yang <yyqengels@gmail.com>
2024-09-09 16:11:01 -0500

    Added mom for multiple passages

--- binomialConvolution.ORA/R/estimate_mom.R
+++ binomialConvolution.ORA/R/estimate_mom.R
@@ -1,31 +1,122 @@
+library(dplyr)
+
+
+
+
+get_passage_moments <- function(passage_data,
+                                moments_to_compute=c("mXp", "vXp", "mYp", "vYp", "cXYp", "Np", "np"),
+                                flat=FALSE)
+{
+  result = vector(mode='list', length = length(moments_to_compute))
+  names(result) = moments_to_compute
+  if("mXp" %in% moments_to_compute)
+  {
+    result$mXp = aggregate(passage_data$X, by=list(passage=passage_data$passage), FUN=mean)$x
+  }
+  if("vXp" %in% moments_to_compute)
+  {
+    result$vXp = aggregate(passage_data$X, by=list(passage=passage_data$passage), FUN=var)$x
+  }
+  if("mYp" %in% moments_to_compute)
+  {
+    result$mYp = aggregate(passage_data$Y, by=list(passage=passage_data$passage), FUN=mean)$x
+  }
+  if("vYp" %in% moments_to_compute)
+  {
+    result$vYp = aggregate(passage_data$Y, by=list(passage=passage_data$passage), FUN=var)$x
+  }
+  if("Np" %in% moments_to_compute)
+  {
+    result$Np = aggregate(passage_data$N, by=list(passage=passage_data$passage), FUN=function(x)x[1])$x
+  }
+  if("np" %in% moments_to_compute)
+  {
+    result$np = aggregate(passage_data$passage, by=list(passag=passage_data$passage), FUN=function(x)length(x))$x
+  }
+  if('cXYp' %in% moments_to_compute)
+  {
+    cov = passage_data %>% group_by(passage) %>% summarise(sig=cov(X,Y))
+    result$cXYp = cov$sig
+  }
+  if(flat)
+  {
+    return(unlist(result))
+  }else{
+    return(result)
+  }
+
+}
+
 
 estimate_mom <- function(passage_data)
 {
   Y = passage_data$Y
   X = passage_data$X
-  N = passage_data$N[1]
-  n = nrow(passage_data)
+  P = length(unique(passage_data$passage))
+  H = diag(P)/P - matrix(1, nrow=P, ncol=P)/(P^2)
 
-  true_positive_prob = mean(Y)/N + cov(X,Y)/var(X)*(1-mean(X)/N)
-  true_negative_prob = (1-mean(Y)/N) + cov(X,Y)/var(X)*mean(X)/N
+  passage_sample_moments = get_passage_moments(passage_data=passage_data,
+                                               moments_to_compute=c("mXp", "vXp", 'Np', "np"))
+
+  mX = mean(passage_sample_moments$mXp)
+  N = mean(passage_sample_moments$Np)
+
+  vX = mean(passage_sample_moments$vXp)
+  sigma1 = as.numeric(vX + t(passage_sample_moments$mXp) %*% H %*% (passage_sample_moments$mXp))
+  sigma2 = as.numeric(t(passage_sample_moments$mXp) %*% H %*% (passage_sample_moments$Np-passage_sample_moments$mXp) - vX)
+
+  cXY = cov(X,Y)
+  mY = mean(Y)
+
+  true_negative_prob = 1 - (mY-cXY*mX/sigma1)/(N-mX*(1+sigma2/sigma1))
+  true_positive_prob = cXY/sigma1 - (sigma2/sigma1)*(1-true_negative_prob)
+
+  # true_positive_prob = mY/N + cXY/vX*(1-mX/N)
+  # true_negative_prob = (1-mY/N) + cXY/vX*mX/N
   prob_est = c(true_positive_prob, true_negative_prob)
 
-  mX = mean(X)
-  mY = mean(Y)
-  vX = var(X)
-  cXY = cov(X,Y)
-  data_omega = cbind(X,Y,(X-mX)^2,(X-mX)*(Y-mY))
-  Omega = cov(data_omega)
+  if(P > 1)
+  {
+    bootstrap_sample = bootstrap_passages(passage_data=passage_data,
+                                          true_positive_prob=NA,
+                                          true_negative_prob=NA,
+                                          sample_prob=1)
+    mXs = bind_rows(lapply(bootstrap_sample, FUN=get_passage_moments, moments_to_compute=c("mXp"), flat=T))
+    temp = lapply(bootstrap_sample, FUN=get_passage_moments, moments_to_compute=c("vXp"), flat=T)
+    vXs = sapply(temp, FUN=mean)
+    mYs = sapply(bootstrap_sample, FUN=function(df) {return(mean(df$Y))})
+    cXYs = sapply(bootstrap_sample, FUN=function(df) {return(cov(df$X, df$Y))})
+    data_omega = cbind(mXs, vXs, mYs, cXYs)
+    Omega = cov(data_omega)
 
-  A = rbind(
-    c(-1/N*cXY/vX, 1/N, -cXY/vX^2*(1-mX/N),1/vX*(1-mX/N)),
-    c(1/N*cXY/vX, -1/N, -cXY/vX^2*mX/N,1/vX*mX/N)
-  )
+    partial_tn_mX=-(-cXY*(1/(P*sigma1)-2*mX*(t(passage_sample_moments$mXp)%*%H)/(sigma1^2))/(N-mX*(1-sigma2/sigma1)) +
+      (mY-cXY*mX/sigma1)/(-(N-mX*(1-sigma2/sigma1))^2)*(-(1/P*(1+sigma2/sigma1)+mX*((t(passage_sample_moments$Np)%*%H-2*t(passage_sample_moments$mXp)%*%H)/sigma1-
+                                                                                      2*sigma2/(sigma1)^2*t(passage_sample_moments$mXp)%*%H))))
+    partial_tp_mX=-2*cXY*(t(passage_sample_moments$mXp)%*%H)/(sigma1^2) - (((t(passage_sample_moments$Np)%*%H-2*t(passage_sample_moments$mXp)%*%H)/sigma1-
+                                                                               2*sigma2/(sigma1)^2*t(passage_sample_moments$mXp)%*%H)*((mY-cXY*mX/sigma1)/(N-mX*(1+sigma2/sigma1)))+
+                                                                               (sigma2/sigma1)*(-partial_tn_mX))
 
-  CV = A %*% Omega %*% t(A) / n
+    partial_tn_vX=-(cXY*mX/(sigma1^2)/(N-mX*(1+sigma2/sigma1))-((mY-cXY*mX/sigma1)/(N-mX*(1+sigma2/sigma1))^2)*(mX*(1/sigma1+sigma2/(sigma1^2))))
+    partial_tp_vX=-cXY/(sigma1^2)-((-1/sigma1-sigma2/(sigma1)^2)*(mY-cXY*mX/sigma1)/(N-mX*(1+sigma2/sigma1)) + (sigma2/sigma1)*(-partial_tn_vX))
 
+    partial_tn_mY = -1/(N-mX*(1+sigma2/sigma1))
+    partial_tp_mY = sigma2/sigma1*partial_tn_mY
+
+    partial_tn_cXY = mX/sigma1/(N-mX*(1+sigma2/sigma1))
+    partial_tp_cXY = 1/sigma1+sigma2/sigma1*partial_tn_cXY
+
+    A = rbind(c(partial_tp_mX, partial_tp_vX, partial_tp_mY, partial_tp_cXY),
+              c(partial_tn_mX, partial_tn_vX, partial_tn_mY, partial_tn_cXY))
+  }else{
+    data_omega = cbind(X,Y,(X-mX)^2,(X-mX)*(Y-mY))
+    Omega = cov(data_omega)/np
+    A = rbind(
+      c(-1/N*cXY/vX, 1/N, -cXY/vX^2*(1-mX/N),1/vX*(1-mX/N)),
+      c(1/N*cXY/vX, -1/N, -cXY/vX^2*mX/N,1/vX*mX/N)
+    )
+  }
+  CV = A %*% Omega %*% t(A)
   SE = sqrt(diag(CV))
-
   result = c(prob_est, SE)
   return(result)
 }
@@ -37,33 +128,53 @@ estimate_gmm_helper <- function(par,passage_data,Emp.Mom,invOmega)
 
   # data must be structured as a list with array items X and Y
   # and scalar items N and n
+  Y = passage_data$Y
+  X = passage_data$X
+  N = unname(get_passage_moments(passage_data, "Np", flat=T))
 
-  mu.X = par[1]
-  var.X = par[2]
-  pi.tp = par[3]
-  pi.tn = par[4]
+
+  n_nuisance_par = length(par)-2
+  mu.X = unname(par[1:(n_nuisance_par/2)])
+  var.X = unname(par[(n_nuisance_par/2+1):n_nuisance_par])
+  pi.tp = par[n_nuisance_par+1]
+  pi.tn = par[n_nuisance_par+2]
 
   mu.Y = mu.X * pi.tp + (N - mu.X) * (1 - pi.tn)
   var.Y = mu.X * pi.tp * (1 - pi.tp) + (N - mu.X) * pi.tn * (1 - pi.tn) +
     var.X * (pi.tp + pi.tn - 1)^2
   cov.XY = var.X * (pi.tp + pi.tn - 1)
 
-  Y = passage_data$Y
-  X = passage_data$X
-  N = passage_data$N[1]
+
+
   n = nrow(passage_data)
 
   if (missing(Emp.Mom)) {
-    Emp.Mom = c(mean(X),var(X),mean(Y),var(Y),cov(X,Y))
+    Emp.Mom = unname(get_passage_moments(passage_data,
+                                  moments_to_compute=c("mXp", "vXp", "mYp", 'vYp', 'cXYp'), flat=T))
+    # Emp.Mom = c(mean(X),var(X),mean(Y),var(Y),cov(X,Y))
     #Emp.Mom = c(mean(X),var(X),mean(Y),cov(X,Y))
   }
 
   if (missing(invOmega)) {
-    mX = mean(X)
-    mY = mean(Y)
-    n = length(X)
-    data.omega = cbind(X,(X-mX)^2,Y,(Y-mY)^2,(X-mX)*(Y-mY))
-    Omega = cov(data.omega)
+
+    if(P > 1)
+    {
+      bootstrap_sample = bootstrap_passages(passage_data=passage_data,
+                                            true_positive_prob=NA,
+                                            true_negative_prob=NA,
+                                            sample_prob=1)
+      data.omega = bind_rows(lapply(bootstrap_sample,
+                                    FUN=get_passage_moments,
+                                    moments_to_compute=c("mXp", "vXp", "mYp", 'vYp', 'cXYp'),
+                                    flat=T))
+      Omega = cov(data.omega)
+    }else{
+      mX = mean(X)
+      mY = mean(Y)
+      n = length(X)
+      data.omega = cbind(X,(X-mX)^2,Y,(Y-mY)^2,(X-mX)*(Y-mY))
+      Omega = cov(data.omega)
+    }
     invOmega = solve(Omega)
   }
 
@@ -79,12 +190,17 @@ estimate_gmm_helper <- function(par,passage_data,Emp.Mom,invOmega)
 estimate_gmm <- function(passage_data)
 {
   mom_est = estimate_mom(passage_data)
-  gmm_est = optim(c(mean(X),var(X),mom.ests$pi.hat),
+
+  P = length(unique(passage_data$passage))
+
+  gmm_est = optim(c(get_passage_moments(passage_data, "mXp",T),
+                    get_passage_moments(passage_data, "vXp",T),
+                    mom.ests$pi.hat),
                     fn = estimate_gmm_helper,
                     passage_data = passage_data,
                     method = "L-BFGS-B",
-                    lower = c(0,0,0,0),
-                    upper = c(passage_data$N[1],Inf,1,1),
+                    lower = c(rep(0, 2*P),0,0),
+                    upper = c(get_passage_moments(passage_data, "Np",T),rep(Inf, P),1,1),
                     hessian = TRUE)
   result = c(gmm_est$par[c(3,4)], sqrt(diag(solve(gmm_est$hessian)))[c(3,4)])
   return(result)
--- binomialConvolution.ORA/R/utility.R
+++ binomialConvolution.ORA/R/utility.R
@@ -85,3 +85,98 @@ generate_counts <- function(positive_prob,
 }
 
 
+
+resample_passage <- function(passage_data,
+                              true_positive_prob=NA,
+                              true_negative_prob=NA,
+                              passage_name="1",
+                              sample_prob=NA)
+{
+  passage_data = passage_data[which(passage_data$passage == passage_name), ]
+  n_students = nrow(passage_data)
+  n_words = passage_data$N[1]
+
+  if(is.na(true_positive_prob) | is.na(true_negative_prob))
+  {
+    method = "m-out-of-n"
+    m=floor(n_students * sample_prob)
+  }else{
+    method = "semi-parametric"
+    m=n_students
+  }
+
+  ind=sample(n_students, size=m, replace=TRUE)
+
+  n_positive=passage_data$X[ind]
+  if(method=="semi-parametric")
+  {
+    true_positive_par_list = generate_counts_par(n_students=n_students,
+                                                 size=n_positive,
+                                                 prob=true_positive_prob,
+                                                 overdispersion=0)
+    n_true_positive = do.call(what=true_positive_par_list$fun_name,
+                              args=true_positive_par_list$fun_args)
+
+    false_positive_par_list = generate_counts_par(n_students=n_students,
+                                                  size=n_words-n_positive,
+                                                  prob=1-true_negative_prob,
+                                                  overdispersion=0)
+    n_false_positive = do.call(what=false_positive_par_list$fun_name,
+                               args=false_positive_par_list$fun_args)
+
+    observation = n_true_positive+n_false_positive
+  }else{
+    observation = passage_data$Y[ind]
+  }
+
+  result = data.frame("passage"=passage_name,
+                      "X"=n_positive,
+                      "Y"=observation,
+                      "N"=n_words,
+                      "n_positive"=n_positive,
+                      "n_negative"=n_words-n_positive)
+
+  return(result)
+}
+
+
+resample_passages <- function(passage_data,
+                               true_positive_prob=NA,
+                               true_negative_prob=NA,
+                               sample_prob=NA)
+{
+  passage_name = unique(passage_data$passage)
+  result = c()
+  for(p_name in passage_name)
+  {
+    p_data = resample_passage(passage_data=passage_data,
+                               true_positive_prob=true_positive_prob,
+                               true_negative_prob=true_negative_prob,
+                               passage_name=p_name,
+                               sample_prob=sample_prob)
+    result = rbind(result, p_data)
+  }
+  return(result)
+}
+
+
+
+bootstrap_passages <- function(passage_data,
+                               true_positive_prob=NA,
+                               true_negative_prob=NA,
+                               n_bootstrap=50,
+                               sample_prob=NA)
+{
+  result_list = list()
+  for(b in 1 : n_bootstrap)
+  {
+    result_list[[b]] = resample_passages(passage_data=passage_data,
+                                         true_positive_prob=true_positive_prob,
+                                         true_negative_prob=true_negative_prob,
+                                         sample_prob=sample_prob)
+  }
+  return(result_list)
+}
+
+
+
